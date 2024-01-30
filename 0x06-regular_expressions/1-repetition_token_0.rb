c5a59d76a178d6b71e3eded5d2744afc1054d964
leoncepromus <twayinganyiki.l@gmail.com>
2024-01-30 06:03:37 -0800

    1-repetition_token_0.rb

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,14 +1,2 @@
 #!/usr/bin/env ruby
-
-# Get the argument passed to the script
-input = ARGV[0]
-
-# Define the regular expression pattern using repetition token
-pattern = /hbt{2,5}n/
-
-# Check if the input matches the pattern
-if input =~ pattern
-  puts input
-else
-  puts '$'
-end
+puts ARGV[0].scan(/hbt{2,5}n/).join
